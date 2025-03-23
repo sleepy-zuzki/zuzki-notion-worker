86e005a9e8c12aaf7b883321abb9e60ee793dddc
Zuzky <sleepyzuzki@gmail.com>
2025-03-23 16:53:33 -0500

    Added notion client

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
-
+import { fetchOverlays } from './api/notion';
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.use(renderer)
 
 app.get('/', (c) => {
   return c.render(<h1>Hello!</h1>)
+})
+
+app.get('/overlays', async (c) => {
+  return c.json(await fetchOverlays(c.env?.NOTION_SECRET))
 })
 
 export default app
