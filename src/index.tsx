09822da37a7625d99f1a9afb10f1d0fd629e1c13
Zuzky <sleepyzuzki@gmail.com>
2025-03-29 16:51:12 -0500

    updated cors rules

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,14 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
 import { fetchOverlays } from './api/notion';
+import { cors } from 'hono/cors'
+
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
-app.use(renderer)
+app.use(renderer, cors({
+  origin: ['https://api.zuzki.dev', 'http://localhost:4200'],
+  allowMethods: ['GET', 'OPTIONS']
+}))
 
 app.get('/', (c) => {
   return c.render(<h1>Hello!</h1>)
