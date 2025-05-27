cb5fe7ecef44c5b50fd45cd917a5ccbe3c482ee1
Zuzki <sleepy@zuzki.dev>
2025-05-26 19:45:24 -0500

    Add new localhost port to CORS origin whitelist
    Included `http://localhost:8788` in the CORS `origin` array to support local development on an additional port. This change ensures requests from the new development environment are not blocked.

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ interface Overlay {
 // Constants
 const API_URL = "https://github.zuzki.dev/data/";
 const CORS_CONFIG = {
-  origin: ['https://www.zuzki.dev', 'https://zuzki.dev', 'https://github.zuzki.dev', 'http://localhost:4200'],
+  origin: ['https://www.zuzki.dev', 'https://zuzki.dev', 'https://github.zuzki.dev', 'http://localhost:4200', 'http://localhost:8788'],
   allowMethods: ['POST', 'GET', 'OPTIONS'],
   exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
   maxAge: 600,
